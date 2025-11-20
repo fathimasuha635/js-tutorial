8bdc5822d2669d9a0b54a0cf9e635daa74d98116
fathimasuha635 <fathimasuha635>
2025-11-20 20:11:14 +0530

    added ifconditions

---
+++ ifcondition.js
@@ -0,0 +1,16 @@
+const user = {
+  email: 'suha',
+  age: 17,
+  gender: 'female'
+}
+
+// if (user.age >= 18) {
+//   console.log(true)
+// } else if (user.age === 15) {
+//   console.log(true)
+// } else {
+//   console.log("Your age is less than 18")
+// }
+
+// console.log("your age is:", user.age);
+console.log(`your gender is ${user.gender}`);
