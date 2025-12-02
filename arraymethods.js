a9d01c64f14e1a265a4835e505623c04876745cc
fathimasuha635 <fathimasuha635>
2025-12-02 20:19:01 +0530

    addedArraymethods

---
+++ arraymethods.js
@@ -0,0 +1,35 @@
+// Push Method
+
+// const animals = ["pigs", "goats", "sheep"];
+// animals.push("dog")
+// console.log(animals);
+
+// Every Method
+
+// const isBelowThreshold = currentValue => {
+//   if (currentValue < 40) {
+//     return true
+//   }
+// }
+
+// const array1 = [1, 30, 39, 29, 10, 13]
+
+// console.log(array1.every(isBelowThreshold))
+
+// Filter Method
+
+// const a = ['spray', 18, 'exuberant', 0, 'present']
+// let res = []
+
+// const result = a.filter((x, y) => {
+//   if (typeof x === 'number') {
+//     res.push(x)
+//   }
+// })
+
+// console.log(res)
+
+// sorting method
+const a = [3, 9, 0, 15]
+const sorted = a.sort((x, y) => x - y)
+console.log(sorted)
